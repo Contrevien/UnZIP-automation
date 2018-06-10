ca10caf71e34eb271bef97e9897b51d572288d67
Akhand Mishra <akkimysite@gmail.com>
2018-06-11 04:15:05 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from cx_Freeze import setup, Executable
+
+base = None    
+
+executables = [Executable("final.py", base=base)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {    
+        'packages':packages,
+    },    
+}
+
+setup(
+    name = "ZIP Automation",
+    options = options,
+    version = "1.0",
+    description = 'An automated software to extract any ZIP file as soon as it is downloaded',
+    executables = executables
+)
+
